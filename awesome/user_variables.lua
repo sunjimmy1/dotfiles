f3fbe8422579ae466a0bd2620b202ecb59051603
Jimmy Sunbury <sunbur94@students.rowan.edu>
2022-09-21 13:22:05 -0400

    Initial Commit

---
+++ awesome/user_variables.lua
@@ -0,0 +1,18 @@
+return {
+	widget = {
+		weather = {
+			--- API Key
+			key = "185b84907131764f04601a8bde2d76b1",
+			--- Coordinates
+			coordinates = {
+				"39.843822", --- lat
+				"-74.744286", --- lon
+			},
+		},
+
+		--- Github activity
+		github = {
+			username = "sunjimmy1",
+		},
+	},
+}
